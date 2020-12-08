32b19d1f116b5d0b8989979a5f46211d110414b8
shane <hunghsuyong114shane@gmail.com>
2020-12-08 12:21:39 +0800

    change in check_data.py

--- src/acoustic/src/plot_streaming_data/check_data.py
+++ src/acoustic/src/plot_streaming_data/check_data.py
@@ -18,7 +18,7 @@ class check_data_node:
         rospy.Subscriber("/get_sound_data_for2i2/hydrophone_data", HydrophoneData, self.push)
     
     # define a callback function to push hydrophone data to container 
-    def push(self):
+    def push(self, data):
         self.data_ch1 = np.concatenate((self.data_ch1, data.data_ch1))
         self.data_ch2 = np.concatenate((self.data_ch2, data.data_ch2))
         self.fs = data.fs
@@ -32,9 +32,10 @@ def main():
     rospy.init_node("check_data_node", anonymous=True)
     node = check_data_node()
     while not rospy.is_shutdown():
-        print("lenght of the data ch1: ", len(node.data_ch1))
-        print("the max of the data ch1: ", np.max(node.data_ch1))
-        print("the min of the data ch1: ", np.min(node.data_ch2))
+        if(len(node.data_ch1)):
+            print("lenght of the data ch1: ", len(node.data_ch1))
+            print("the max of the data ch1: ", np.max(node.data_ch1))
+            print("the min of the data ch1: ", np.min(node.data_ch1))
         time.sleep(5)
 if __name__ == "__main__":
     main()
