9bba9992ce45785703798a1623afa2c733108bd5
Shane Hung <hunghsuyong114shane@gmail.com>
2021-06-25 18:39:12 +0800

    modified after field test

--- script/buoy.sh
+++ script/buoy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #IP=$(ifconfig | grep wl -A 1 | grep inet | awk '{print $2}')
 source /home/ual/ros_buoy_ws/devel/setup.bash
-IP=192.168.2.101
+IP=192.168.2.102
 SHORESIDE_IP=192.168.2.103
 export ROS_IP=$IP
 #export ROS_MASTER_URI=http://192.168.1.102:11311
