9704f6a01b1aead47bbdb4f7f01efd9478f89a3e
Shane Hung <hunghsuyong114shane@gmail.com>
2021-09-04 14:38:42 +0800

    update for mission

--- script/buoy.sh
+++ script/buoy.sh
@@ -5,7 +5,7 @@ IP=192.168.2.102
 SHORESIDE_IP=192.168.2.100
 export ROS_IP=$IP
 #export ROS_MASTER_URI=http://192.168.1.102:11311
-export ROS_MASTER_URI=http://$IP:11311
+export ROS_MASTER_URI=http://$SHORESIDE_IP:11311
 ifconfig
 printenv | grep ROS
 sudo sh -c "echo 0 >/proc/sys/net/ipv4/icmp_echo_ignore_broadcasts"
